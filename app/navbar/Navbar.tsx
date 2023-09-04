e209141846421fac64249294a948e6bfcd394e94
Dara Edmondson <daraedmondson@Daras-MacBook-Pro.local>
2023-09-04 22:24:48 +0100

    Change navbar style

--- app/navbar/Navbar.tsx
+++ app/navbar/Navbar.tsx
@@ -2,11 +2,19 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">Home</Link>
-      <Link href="/submit">Form</Link>
-      <Link href="/about">About</Link>
-    </div>
+    <nav className="w-full bg-slate-800 flex flew-wrap flex-row items-center justify-between">
+      <Link href="/">
+        <span className="text-2xl self-center whitespace-nowrap">Quollab</span>
+      </Link>
+      <div className="md:flex-row md:space-x-2">
+        <Link href="/submit" className="hover:text-white">
+          Submit
+        </Link>
+        <Link href="/about" className="hover:text-white">
+          About
+        </Link>
+      </div>
+    </nav>
   );
 };
 
