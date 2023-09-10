d87d333abaa24b24fb9d5c346ea61e09ba0c0a6a
Dara Edmondson <daraedmondson@Daras-MacBook-Pro.local>
2023-09-10 23:59:06 +0100

    Update about us page, remove navbar links

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,28 @@
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+    <div className="flex flex-col gap-5 py-5 px-10">
+      <div className="flex flex-col gap-5">
+        <h1>About Us</h1>
+        <p className="px-5">
+          <b>Quollab</b> is a collaboration platform designed exclusively for
+          aspiring researchers
+        </p>
+        <p className="px-5">
+          Our mission is to bridge the gap between the next generation of
+          medical professionals and the world of research, enabling students to
+          build their portfolios and contribute meaningfully to academia
+        </p>
+      </div>
+      <div className="flex flex-col gap-5">
+        <h1>Our Vision</h1>
+        <p className="px-5">
+          We envision a future where every student has the opportunity to
+          actively engage in groundbreaking research, gain valuable hands-on
+          experience, and make a lasting impact. We believe that connecting
+          students with researchers is the key to nurturing talent, fostering
+          innovation, and advancing science
+        </p>
+      </div>
     </div>
   );
 };
--- app/navbar/Navbar.tsx
+++ app/navbar/Navbar.tsx
@@ -4,139 +4,113 @@ import { Transition } from "@headlessui/react";
 import Link from "next/link";
 import { Dispatch, SetStateAction, useState } from "react";
 
-const BUTTONS = [
-	{ text: "Submit", href: "/submit" },
-	{ text: "About", href: "/about" },
-];
-
-const SIGNUP_BUTTONS = [
-	{ text: "Sign Up", href: "#" },
-	{ text: "Log In", href: "#" },
-];
+const BUTTONS = [{ text: "About Us", href: "/about" }];
 
 const Hamburger = (props: {
-	isOpen: boolean;
-	setIsOpen: Dispatch<SetStateAction<boolean>>;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 }) => {
-	const genericLine =
-		"h-1 w-6 my-1 rounded-full bg-slate-500 transition ease transform duration-300";
+  const genericLine =
+    "h-1 w-6 my-1 rounded-full bg-slate-500 transition ease transform duration-300";
 
-	return (
-		<button
-			className="flex flex-col h-12 w-12 justify-center items-center group"
-			onClick={() => props.setIsOpen(!props.isOpen)}
-			role="menu"
-		>
-			<span
-				className={`${genericLine} ${
-					props.isOpen
-						? "rotate-45 translate-y-3 opacity-50 group-hover:opacity-100"
-						: "opacity-50 group-hover:opacity-100"
-				}`}
-			/>
-			<span
-				className={`${genericLine} ${
-					props.isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"
-				}`}
-			/>
-			<span
-				className={`${genericLine} ${
-					props.isOpen
-						? "-rotate-45 -translate-y-3 opacity-50 group-hover:opacity-100"
-						: "opacity-50 group-hover:opacity-100"
-				}`}
-			/>
-		</button>
-	);
+  return (
+    <button
+      className="flex flex-col h-12 w-12 justify-center items-center group"
+      onClick={() => props.setIsOpen(!props.isOpen)}
+      role="menu"
+    >
+      <span
+        className={`${genericLine} ${
+          props.isOpen
+            ? "rotate-45 translate-y-3 opacity-50 group-hover:opacity-100"
+            : "opacity-50 group-hover:opacity-100"
+        }`}
+      />
+      <span
+        className={`${genericLine} ${
+          props.isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"
+        }`}
+      />
+      <span
+        className={`${genericLine} ${
+          props.isOpen
+            ? "-rotate-45 -translate-y-3 opacity-50 group-hover:opacity-100"
+            : "opacity-50 group-hover:opacity-100"
+        }`}
+      />
+    </button>
+  );
 };
 
 const NavBarLink = (props: {
-	text: string;
-	href: string;
-	isOpen: boolean;
-	setIsOpen: Dispatch<SetStateAction<boolean>>;
-	other?: any[];
+  text: string;
+  href: string;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  other?: any[];
 }) => {
-	return (
-		<Link
-			className="p-2 hover:text-white"
-			href={props.href}
-			onClick={() => props.setIsOpen(!props.isOpen)}
-			{...props.other}
-		>
-			{props.text}
-		</Link>
-	);
+  return (
+    <Link
+      className="p-2 hover:text-white"
+      href={props.href}
+      onClick={() => props.setIsOpen(!props.isOpen)}
+      {...props.other}
+    >
+      {props.text}
+    </Link>
+  );
 };
 
 const Navbar = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	return (
-		<nav className="sticky top-0 w-full z-50 bg-slate-800 flex flew-wrap flex-row items-center justify-between px-1 text-slate-500">
-			<div className="flex flex-row">
-				<Link
-					className="py-2 px-1 hover:text-white"
-					href="/"
-					onClick={() => setIsOpen(!isOpen)}
-				>
-					<span className="text-2xl self-center whitespace-nowrap">
-						Quollab
-					</span>
-				</Link>
-				<div className="hidden sm:flex items-center sm:flex-row sm:space-x-2">
-					{BUTTONS.map((obj) => (
-						<NavBarLink
-							isOpen={isOpen}
-							setIsOpen={setIsOpen}
-							{...obj}
-							key={obj.text}
-						/>
-					))}
-				</div>
-			</div>
-			<div className="hidden sm:flex items-center sm:flex-row sm:space-x-2">
-				{SIGNUP_BUTTONS.map((obj) => (
-					<NavBarLink
-						isOpen={isOpen}
-						setIsOpen={setIsOpen}
-						{...obj}
-						key={obj.text}
-					/>
-				))}
-			</div>
-			<div className="sm:hidden">
-				<Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
-				<Transition
-					enter="transition duration-100 ease-out"
-					enterFrom="transform scale-95 opacity-0"
-					enterTo="transform scale-100 opacity-100"
-					leave="transition duration-75 ease-out"
-					leaveFrom="transform scale-100 opacity-100"
-					leaveTo="transform scale-95 opacity-0"
-					show={isOpen}
-				>
-					<div className="absolute flex flex-col right-0 mt-1 origin-top-right rounded-md bg-slate-800 px-5 w-screen">
-						{BUTTONS.map((obj) => (
-							<NavBarLink
-								isOpen={isOpen}
-								setIsOpen={setIsOpen}
-								{...obj}
-								key={obj.text}
-							/>
-						))}
-						{SIGNUP_BUTTONS.map((obj) => (
-							<NavBarLink
-								isOpen={isOpen}
-								setIsOpen={setIsOpen}
-								{...obj}
-								key={obj.text}
-							/>
-						))}
-					</div>
-				</Transition>
-			</div>
-		</nav>
-	);
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className="sticky top-0 w-full z-50 bg-slate-800 flex flew-wrap flex-row items-center justify-between px-1 text-slate-500">
+      <div className="flex flex-row">
+        <Link
+          className="py-2 px-1 hover:text-white"
+          href="/"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <span className="text-2xl self-center whitespace-nowrap">
+            Quollab
+          </span>
+        </Link>
+      </div>
+      <div className="hidden sm:flex items-center sm:flex-row sm:space-x-2">
+        {BUTTONS.map((obj) => (
+          <NavBarLink
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            {...obj}
+            key={obj.text}
+          />
+        ))}
+      </div>
+      <div className="sm:hidden">
+        <Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
+        <Transition
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+          show={isOpen}
+        >
+          <div className="absolute flex flex-col right-0 mt-1 origin-top-right rounded-md bg-slate-800 px-5 w-screen">
+            {BUTTONS.map((obj) => (
+              <NavBarLink
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+                {...obj}
+                key={obj.text}
+              />
+            ))}
+          </div>
+        </Transition>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
