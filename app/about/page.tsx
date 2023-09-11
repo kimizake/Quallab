28c396a9a030b03033555e510eca642b3a245849
mikiivanovic <mikiivanovic@icloud.com>
2023-09-11 13:00:27 +0100

    Update logo and name

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,30 +1,30 @@
 const About = () => {
-  return (
-    <div className="flex flex-col gap-5 py-5 px-10">
-      <div className="flex flex-col gap-5">
-        <h1>About Us</h1>
-        <p className="px-5">
-          <b>Quollab</b> is a collaboration platform designed exclusively for
-          aspiring researchers
-        </p>
-        <p className="px-5">
-          Our mission is to bridge the gap between the next generation of
-          medical professionals and the world of research, enabling students to
-          build their portfolios and contribute meaningfully to academia
-        </p>
-      </div>
-      <div className="flex flex-col gap-5">
-        <h1>Our Vision</h1>
-        <p className="px-5">
-          We envision a future where every student has the opportunity to
-          actively engage in groundbreaking research, gain valuable hands-on
-          experience, and make a lasting impact. We believe that connecting
-          students with researchers is the key to nurturing talent, fostering
-          innovation, and advancing science
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-5 py-5 px-10">
+			<div className="flex flex-col gap-5">
+				<h1>About Us</h1>
+				<p className="px-5">
+					<b>Quallab</b> is a collaboration platform designed exclusively for
+					aspiring researchers
+				</p>
+				<p className="px-5">
+					Our mission is to bridge the gap between the next generation of
+					medical professionals and the world of research, enabling students to
+					build their portfolios and contribute meaningfully to academia
+				</p>
+			</div>
+			<div className="flex flex-col gap-5">
+				<h1>Our Vision</h1>
+				<p className="px-5">
+					We envision a future where every student has the opportunity to
+					actively engage in groundbreaking research, gain valuable hands-on
+					experience, and make a lasting impact. We believe that connecting
+					students with researchers is the key to nurturing talent, fostering
+					innovation, and advancing science
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default About;
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,27 +7,27 @@ import Footer from "./footer/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Quollab",
-  description:
-    "Quollab starter website. Currently contains a dummy landing page, which directs to a dummy form",
+	title: "Quallab",
+	description:
+		"Quallab starter website. Currently contains a dummy landing page, which directs to a dummy form",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.className} flex flex-col h-screen w-full bg-navy text-off-white`}
-      >
-        <Navbar />
-        <div className="flex justify-center flex-row flex-auto mx-2 my-5">
-          {children}
-        </div>
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${inter.className} flex flex-col h-screen w-full bg-navy text-off-white`}
+			>
+				<Navbar />
+				<div className="flex justify-center flex-row flex-auto mx-2 my-5">
+					{children}
+				</div>
+				<Footer />
+			</body>
+		</html>
+	);
 }
