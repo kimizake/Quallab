6626f180433690f4ca302d1c47cccf231298545f
mikiivanovic <mikiivanovic@icloud.com>
2023-09-11 13:06:37 +0100

    Inject logo into home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,41 @@
+import Image from "next/image";
 import { Button } from "./components/Button";
+import * as logo from "../assets/Quallab Grey Logo.png";
 
 export default function Home() {
-  const BUTTONS = [
-    {
-      label: "Find Collaborators",
-      href: "/find-collaborators",
-      text: "Connect with researchers to further your project",
-    },
-    {
-      label: "Find Research Projects",
-      href: "/find-projects",
-      text: "Boost your portfolio",
-    },
-  ];
-  return (
-    <main className="flex flex-col py-5 px-5 gap-10">
-      <div></div>
-      <div className="text-center">
-        Connecting students and researchers to foster meaningful collaboration
-      </div>
-      <div className="grid md:grid-cols-2 md:px-10 gap-6 justify-items-center justify-center">
-        {BUTTONS.map((button) => (
-          <div className="flex flex-col w-full text-center space-y-2 items-center">
-            <Button label={button.label} href={button.href} />
-            <text>{button.text}</text>
-          </div>
-        ))}
-      </div>
-    </main>
-  );
+	const BUTTONS = [
+		{
+			label: "Find Collaborators",
+			href: "/find-collaborators",
+			text: "Connect with researchers to further your project",
+		},
+		{
+			label: "Find Research Projects",
+			href: "/find-projects",
+			text: "Boost your portfolio",
+		},
+	];
+	return (
+		<main className="flex flex-col py-5 px-5 gap-10">
+			<div className="inline-flex justify-center">
+				<Image
+					src={logo}
+					alt={"Logo"}
+					width={logo.default.width}
+					height={logo.default.height}
+				/>
+			</div>
+			<div className="text-center">
+				Connecting students and researchers to foster meaningful collaboration
+			</div>
+			<div className="grid md:grid-cols-2 md:px-10 gap-6 justify-items-center justify-center">
+				{BUTTONS.map((button) => (
+					<div className="flex flex-col w-full text-center space-y-2 items-center">
+						<Button label={button.label} href={button.href} />
+						<text>{button.text}</text>
+					</div>
+				))}
+			</div>
+		</main>
+	);
 }
