c362da937b3de6a68b566139b45603c82fe33d8e
mikiivanovic <mikiivanovic@icloud.com>
2023-10-11 19:03:31 +0200

    Increase radio input click area

--- app/components/RadioPanel.tsx
+++ app/components/RadioPanel.tsx
@@ -13,15 +13,15 @@ export const RadioPanel = (props: {
 			<ul className="items-top w-full text-sm font-medium bg-dark-blue border border-off-white rounded-md sm:flex">
 				{props.buttons.map((button, idx) => (
 					<li key={idx} className="w-full inline-flex items-center py-2 px-1">
-						<input
-							type="radio"
-							value={button.label}
-							name={props.name}
-							className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-							required={props.required}
-							onInput={button.onInput}
-						/>
 						<label className="w-full ml-3 text-sm font-medium">
+							<input
+								type="radio"
+								value={button.label}
+								name={props.name}
+								className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+								required={props.required}
+								onInput={button.onInput}
+							/>
 							{button.label}
 						</label>
 					</li>
