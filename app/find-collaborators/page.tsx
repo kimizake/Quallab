945f58ef5cff3b9e5965b21105a6d7b95d9a8fa4
mikiivanovic <mikiivanovic@icloud.com>
2023-10-11 18:49:49 +0200

    Disable button after form submission and refactor

--- app/find-collaborators/page.tsx
+++ app/find-collaborators/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ChangeEvent, FormEvent, useReducer, useState } from "react";
+import { ChangeEvent, FormEvent, Fragment, useReducer, useState } from "react";
 import { Input } from "../components/Input";
 import { RadioPanel } from "../components/RadioPanel";
 import { Select } from "../components/Select";
@@ -29,7 +29,7 @@ const formReducer = (
 	};
 };
 
-const FindCollaborators = () => {
+const FormBody = (props: { enabled: boolean }) => {
 	const [otherSpecialty, toggleOtherSpecialty] = useState(false);
 	const [accessRequirements, setAccessRequirements] = useState(false);
 	const [experienceDetails, setExperienceDetails] = useState(false);
@@ -51,7 +51,188 @@ const FindCollaborators = () => {
 		"Public health",
 		"Surgery",
 	];
+	return (
+		<Fragment>
+			<div className="grid md:grid-cols-2 gap-5 md:gap-10">
+				<Input
+					label="First Name"
+					type="text"
+					name="firstName"
+					placeholder=""
+					required={true}
+				/>
+				<Input
+					label="Last Name"
+					type="text"
+					name="lastName"
+					placeholder=""
+					required={true}
+				/>
+			</div>
+			<Input
+				label="Insutition Email"
+				type="email"
+				name="email"
+				placeholder=""
+				pattern="[A-za-z]+@ucl.ac.uk"
+				required={true}
+			/>
+			<Select
+				label="Job Title"
+				name="jobTitle"
+				options={JobDropdownOptions}
+				placeholder=""
+				required={true}
+			/>
+			<Select
+				label="Specialty of interest"
+				name="interest"
+				options={SpecialtyDropdownOptions}
+				onChange={(e) => {
+					toggleOtherSpecialty(e.target.value === "Other");
+				}}
+				placeholder=""
+				required={true}
+			/>
+			<div className={otherSpecialty ? "" : "hidden"}>
+				<Input
+					label="Specialty of Interest"
+					name="interest"
+					type="text"
+					placeholder=""
+					required={otherSpecialty}
+				/>
+			</div>
+			<Input
+				label="Title of Project"
+				name="projectTitle"
+				type="text"
+				placeholder=""
+				required={true}
+			/>
+			<Input
+				label="Brief description of project"
+				name="projectDescription"
+				type="text"
+				placeholder=""
+				required={true}
+			/>
+			<Input
+				label="Research style (e.g. literature review, audit)"
+				name="researchStyle"
+				type="text"
+				placeholder=""
+				required={true}
+			/>
+			<TextArea
+				label="Description of work"
+				name="descriptionOfWork"
+				placeholder=""
+				required={true}
+			/>
+			<Input
+				label="Time requirement (estimated hours)"
+				name="timeRequirement"
+				type="number"
+				min={0}
+				placeholder=""
+				required={true}
+			/>
+			<Input
+				label="Research start date requirement"
+				name="startDate"
+				type="date"
+				placeholder=""
+				required={true}
+			/>
+			<Input
+				label="How many collaborators are you looking for"
+				name="numberOfCollaborators"
+				type="number"
+				min={0}
+				placeholder=""
+				required={true}
+			/>
+			<h1 className="p-0">Requirements for researchers</h1>
+			<RadioPanel
+				heading="Availability"
+				name="availability"
+				buttons={[{ label: "In Person" }, { label: "Online" }]}
+				required
+			/>
+			<RadioPanel
+				heading="Access Requirements"
+				name="accessRequirements"
+				buttons={[
+					{
+						label: "Yes",
+						onInput: () => {
+							setAccessRequirements(true);
+						},
+					},
+					{
+						label: "No",
+						onInput: () => {
+							setAccessRequirements(false);
+						},
+					},
+				]}
+				required
+			/>
+			<div className={accessRequirements ? "" : "hidden"}>
+				<TextArea
+					label="Access requirements (detail)"
+					name="accessRequirements"
+					placeholder=""
+					required={accessRequirements}
+				/>
+			</div>
+			<RadioPanel
+				heading="Experience Level"
+				name="experienceLevel"
+				buttons={[
+					{
+						label: "Beginner (No Research Experience)",
+						onInput: () => {
+							setExperienceDetails(false);
+						},
+					},
+					{
+						label: "Intermediate (Some Research Experience",
+						onInput: () => {
+							setExperienceDetails(true);
+						},
+					},
+					{
+						label: "Advanced (looking for specific skills)",
+						onInput: () => {
+							setExperienceDetails(true);
+						},
+					},
+				]}
+				required
+			/>
+			<div className={experienceDetails ? "" : "hidden"}>
+				<Input
+					label="Skill required (detail)"
+					name="experienceLevel"
+					type="text"
+					placeholder=""
+					required={experienceDetails}
+				/>
+			</div>
+			<button
+				type="submit"
+				className="bg-off-white text-navy text-center px-5 py-2 rounded-md hover:bg-dark-blue hover:text-off-white w-full disabled:bg-slate-800 bg:text-slate-600"
+				disabled={!props.enabled}
+			>
+				Submit
+			</button>
+		</Fragment>
+	);
+};
 
+const FindCollaborators = () => {
 	const [formData, setFormData] = useReducer(formReducer, {
 		accessRequirements: "",
 		availability: "Online",
@@ -69,6 +250,7 @@ const FindCollaborators = () => {
 		startDate: "",
 		timeRequirement: 0,
 	});
+	const [enabled, setEnabled] = useState(true);
 
 	return (
 		<div className="flex flex-col gap-5 w-4/5">
@@ -77,185 +259,14 @@ const FindCollaborators = () => {
 			</h1>
 			<form
 				className="grid gap-5"
-				onSubmit={(e) => {
+				onSubmit={async (e) => {
+					setEnabled(false);
 					handleSubmit(e, { ...formData });
+					setEnabled(true);
 				}}
 				onChange={setFormData}
 			>
-				<div className="grid md:grid-cols-2 gap-5 md:gap-10">
-					<Input
-						label="First Name"
-						type="text"
-						name="firstName"
-						placeholder=""
-						required={true}
-					/>
-					<Input
-						label="Last Name"
-						type="text"
-						name="lastName"
-						placeholder=""
-						required={true}
-					/>
-				</div>
-				<Input
-					label="Insutition Email"
-					type="email"
-					name="email"
-					placeholder=""
-					pattern="[A-za-z]+@ucl.ac.uk"
-					required={true}
-				/>
-				<Select
-					label="Job Title"
-					name="jobTitle"
-					options={JobDropdownOptions}
-					placeholder=""
-					required={true}
-				/>
-				<Select
-					label="Specialty of interest"
-					name="interest"
-					options={SpecialtyDropdownOptions}
-					onChange={(e) => {
-						toggleOtherSpecialty(e.target.value === "Other");
-					}}
-					placeholder=""
-					required={true}
-				/>
-				<div className={otherSpecialty ? "" : "hidden"}>
-					<Input
-						label="Specialty of Interest"
-						name="interest"
-						type="text"
-						placeholder=""
-						required={otherSpecialty}
-					/>
-				</div>
-				<Input
-					label="Title of Project"
-					name="projectTitle"
-					type="text"
-					placeholder=""
-					required={true}
-				/>
-				<Input
-					label="Brief description of project"
-					name="projectDescription"
-					type="text"
-					placeholder=""
-					required={true}
-				/>
-				<Input
-					label="Research style (e.g. literature review, audit)"
-					name="researchStyle"
-					type="text"
-					placeholder=""
-					required={true}
-				/>
-				<TextArea
-					label="Description of work"
-					name="descriptionOfWork"
-					placeholder=""
-					required={true}
-				/>
-				<Input
-					label="Time requirement (estimated hours)"
-					name="timeRequirement"
-					type="number"
-					min={0}
-					placeholder=""
-					required={true}
-				/>
-				<Input
-					label="Research start date requirement"
-					name="startDate"
-					type="date"
-					placeholder=""
-					required={true}
-				/>
-				<Input
-					label="How many collaborators are you looking for"
-					name="numberOfCollaborators"
-					type="number"
-					min={0}
-					placeholder=""
-					required={true}
-				/>
-				<h1 className="p-0">Requirements for researchers</h1>
-				<RadioPanel
-					heading="Availability"
-					name="availability"
-					buttons={[{ label: "In Person" }, { label: "Online" }]}
-					required
-				/>
-				<RadioPanel
-					heading="Access Requirements"
-					name="accessRequirements"
-					buttons={[
-						{
-							label: "Yes",
-							onInput: () => {
-								setAccessRequirements(true);
-							},
-						},
-						{
-							label: "No",
-							onInput: () => {
-								setAccessRequirements(false);
-							},
-						},
-					]}
-					required
-				/>
-				<div className={accessRequirements ? "" : "hidden"}>
-					<TextArea
-						label="Access requirements (detail)"
-						name="accessRequirements"
-						placeholder=""
-						required={accessRequirements}
-					/>
-				</div>
-				<RadioPanel
-					heading="Experience Level"
-					name="experienceLevel"
-					buttons={[
-						{
-							label: "Beginner (No Research Experience)",
-							onInput: () => {
-								setExperienceDetails(false);
-							},
-						},
-						{
-							label: "Intermediate (Some Research Experience",
-							onInput: () => {
-								setExperienceDetails(true);
-							},
-						},
-						{
-							label: "Advanced (looking for specific skills)",
-							onInput: () => {
-								setExperienceDetails(true);
-							},
-						},
-					]}
-					required
-				/>
-				<div className={experienceDetails ? "" : "hidden"}>
-					<Input
-						label="Skill required (detail)"
-						name="experienceLevel"
-						type="text"
-						placeholder=""
-						required={experienceDetails}
-					/>
-				</div>
-				<button
-					type="submit"
-					className="bg-off-white text-navy text-center px-5 py-2 rounded-md hover:bg-dark-blue hover:text-off-white w-full"
-				>
-					Submit
-				</button>
+				<FormBody enabled={enabled} />
 			</form>
 		</div>
 	);
--- app/find-projects/page.tsx
+++ app/find-projects/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ChangeEvent, FormEvent, useReducer, useState } from "react";
+import { ChangeEvent, FormEvent, Fragment, useReducer, useState } from "react";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
 import { ProjectForm } from "../types/formDataType";
@@ -27,7 +27,7 @@ const formReducer = (
 	};
 };
 
-const FindProjects = () => {
+const FormBody = (props: { enabled: boolean }) => {
 	const JobDropdownOptions = ["Medical student", "Junior doctor"];
 	const SpecialtyDropdownOptions = [
 		"Anaesthetics",
@@ -47,6 +47,70 @@ const FindProjects = () => {
 	];
 	const [otherSpecialty, toggleOtherSpecialty] = useState(false);
 
+	return (
+		<Fragment>
+			<div className="grid md:grid-cols-2 gap-5 md:gap-10">
+				<Input
+					label="First Name"
+					type="text"
+					name="firstName"
+					placeholder=""
+					required={true}
+				/>
+				<Input
+					label="Last Name"
+					type="text"
+					name="lastName"
+					placeholder=""
+					required={true}
+				/>
+			</div>
+			<Input
+				label="Insutition Email"
+				type="text"
+				name="email"
+				placeholder=""
+				pattern="[A-za-z]+@[A-Za-z]+.ac.uk"
+				required={true}
+			/>
+			<Select
+				label="Job Title"
+				name="jobTitle"
+				options={JobDropdownOptions}
+				placeholder=""
+				required={true}
+			/>
+			<Select
+				label="Specialty of interest"
+				name="interest"
+				options={SpecialtyDropdownOptions}
+				onChange={(e) => {
+					toggleOtherSpecialty(e.target.value === "Other");
+				}}
+				placeholder=""
+				required={true}
+			/>
+			<div className={otherSpecialty ? "" : "hidden"}>
+				<Input
+					label="Specialty of Interest"
+					name="interest"
+					type="text"
+					placeholder=""
+					required={otherSpecialty}
+				/>
+			</div>
+			<button
+				type="submit"
+				className="bg-off-white text-navy text-center px-5 py-2 rounded-md hover:bg-dark-blue hover:text-off-white w-full disabled:bg-slate-800 disabled:text-slate-600"
+				disabled={!props.enabled}
+			>
+				Submit
+			</button>
+		</Fragment>
+	);
+};
+
+const FindProjects = () => {
 	const [formData, setFormData] = useReducer(formReducer, {
 		// accessRequirements: "",
 		// availability: "Online",
@@ -57,6 +121,7 @@ const FindProjects = () => {
 		jobTitle: "",
 		lastName: "",
 	});
+	const [enabled, setEnabled] = useState(true);
 	return (
 		<div className="flex flex-col gap-5 w-4/5">
 			<h1 className="text-center">
@@ -64,65 +129,14 @@ const FindProjects = () => {
 			</h1>
 			<form
 				className="grid gap-5"
-				onSubmit={(e) => handleSubmit(e, { ...formData })}
+				onSubmit={async (e) => {
+					setEnabled(false);
+					await handleSubmit(e, { ...formData });
+					setEnabled(true);
+				}}
 				onChange={setFormData}
 			>
-				<div className="grid md:grid-cols-2 gap-5 md:gap-10">
-					<Input
-						label="First Name"
-						type="text"
-						name="firstName"
-						placeholder=""
-						required={true}
-					/>
-					<Input
-						label="Last Name"
-						type="text"
-						name="lastName"
-						placeholder=""
-						required={true}
-					/>
-				</div>
-				<Input
-					label="Insutition Email"
-					type="text"
-					name="email"
-					placeholder=""
-					pattern="[A-za-z]+@[A-Za-z]+.ac.uk"
-					required={true}
-				/>
-				<Select
-					label="Job Title"
-					name="jobTitle"
-					options={JobDropdownOptions}
-					placeholder=""
-					required={true}
-				/>
-				<Select
-					label="Specialty of interest"
-					name="interest"
-					options={SpecialtyDropdownOptions}
-					onChange={(e) => {
-						toggleOtherSpecialty(e.target.value === "Other");
-					}}
-					placeholder=""
-					required={true}
-				/>
-				<div className={otherSpecialty ? "" : "hidden"}>
-					<Input
-						label="Specialty of Interest"
-						name="interest"
-						type="text"
-						placeholder=""
-						required={otherSpecialty}
-					/>
-				</div>
-				<button
-					type="submit"
-					className="bg-off-white text-navy text-center px-5 py-2 rounded-md hover:bg-dark-blue hover:text-off-white w-full"
-				>
-					Submit
-				</button>
+				<FormBody enabled={enabled} />
 			</form>
 		</div>
 	);
